58bc1027466ab17e3b3ed9cf72564454c1471710
Andrei Banc <andrei.banc00@gmail.com>
2020-09-09 17:41:30 +0300

    Add a New Character with POST

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -24,5 +24,12 @@ namespace dotnet_rpg.Controllers
         {
             return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
+
+        [HttpPost]
+        public IActionResult AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
     }
 }
