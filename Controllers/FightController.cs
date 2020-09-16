f50495ecd6075a65f2359748a0a0d0a6b723e44f
Andrei Banc <andrei.banc00@gmail.com>
2020-09-16 12:09:16 +0300

    Attack with Skills

--- Controllers/FightController.cs
+++ Controllers/FightController.cs
@@ -21,5 +21,11 @@ namespace dotnet_rpg.Controllers
         {
             return Ok(await _fightService.WeaponAttack(request));
         }
+
+        [HttpPost("Skill")]
+        public async Task<IActionResult> SkillAttack(SkillAttackDto request)
+        {
+            return Ok(await _fightService.SkillAttack(request));
+        }
     }
 }
